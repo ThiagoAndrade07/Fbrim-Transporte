fa2c3b66d23a595579c449a9b4ccbb1b11e40dc6
ThiagoAndrade07 <120478086+ThiagoAndrade07@users.noreply.github.com>
2023-10-26 19:08:23 -0300

    Update index.html, create sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
+const btnHeader = document.querySelector('.menu-burger');
 
+btnHeader.addEventListener('click', abreHeader);
+
+let headerOn = document.querySelector('.menu-mobile');
+
+function abreHeader() {
+
+    if (headerOn.hasAttribute("id")) {
+        headerOn.removeAttribute("id", "display-block");
+    }else {
+        headerOn.setAttribute("id", "display-block");
+    }
+
+  }
+
+const btnHeaderFechar = document.querySelector('.btn-fechar');
+
+btnHeaderFechar.addEventListener('click', abreHeader);
